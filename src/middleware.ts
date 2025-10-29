73c29929c6dd28c57f69e12349ee9469e803801f
kiyameh <kiyameh@outlook.com>
2025-10-29 01:15:08 +0100

    Añadida autenticación con clerk

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
+
+const isProtectedRoute = createRouteMatcher(['/protected(.*)'])
+
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { isAuthenticated, redirectToSignIn, userId } = auth()
+
+  if (!isAuthenticated && isProtectedRoute(context.request)) {
+    console.log("Redirecting to signin")
+
+    return redirectToSignIn()
+  }
+});
