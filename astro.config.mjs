526f573e5fe38dbc87b951d2b41f7535412bca99
kiyameh <kiyameh@outlook.com>
2025-11-12 19:52:44 +0100

    Configurado proyecto, instaladas dependencias principales.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
+import { esES } from "@clerk/localizations";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    clerk({
+      localization: esES,
+    }),
+  ],
+
+  adapter: node({
+    mode: "standalone",
+  }),
+
+  output: "server",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ concept/TECHS.md
@@ -0,0 +1,18 @@
+# Technologies
+
+## Framework
+
+Astro
+
+## UI
+
+Shadcn
+TailwindCSS
+
+## Auth
+
+Clerk
+
+## Collections
+
+Mdx integration
