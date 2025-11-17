ac9d65cfba680b70411d67e893272dd7777468ac
Kiyameh <kiyameh@outlook.com>
2025-11-17 13:10:34 +0100

    Añadida colección autores

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,24 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, reference } from "astro:content";
+
+// Colección de autores
+const authors = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      bio: z.string(),
+      avatar: image(),
+      email: z.string().email().optional(),
+      website: z.string().url().optional(),
+      social: z
+        .object({
+          instagram: z.string().optional(),
+          facebook: z.string().optional(),
+          twitter: z.string().optional(),
+        })
+        .optional(),
+    }),
+});
 
 // Colección de posts del blog
 const posts = defineCollection({
@@ -11,9 +31,11 @@ const posts = defineCollection({
       heroImage: image(),
       categories: z.array(z.string()),
       draft: z.boolean().default(false),
+      author: reference("authors"),
     }),
 });
 
 export const collections = {
   posts,
+  authors,
 };
